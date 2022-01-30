c9c5b284a46dbe81c06066443bce3e49f7ea7a8b
Adel @ LAPTOP-6OHQQOJF <adeltaiebtamacha@gmail.com>
2022-01-30 12:49:22 +0100

    Refactor

--- app/src/main/java/com/example/codesave/MainActivity.java
+++ app/src/main/java/com/example/codesave/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.core.content.ContextCompat;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,71 +16,71 @@ import android.widget.Toast;
 
 import java.util.concurrent.Executor;
 
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "codeSave";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialising msgtext and loginbutton
-        TextView msgtex = findViewById(R.id.msgtext);
-        final Button loginbutton = findViewById(R.id.login);
-
-        // creating a variable for our BiometricManager
-        // and lets check if our user can use biometric sensor or not
+        // Check biometric
         BiometricManager biometricManager = androidx.biometric.BiometricManager.from(this);
         switch (biometricManager.canAuthenticate()) {
 
-            // this means we can use biometric sensor
             case BiometricManager.BIOMETRIC_SUCCESS:
-                msgtex.setText("You can use the fingerprint sensor to login");
-                msgtex.setTextColor(Color.parseColor("#fafafa"));
+                Log.d(TAG, "Biometric active");
                 break;
 
-            // this means that the device doesn't have fingerprint sensor
             case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
-                msgtex.setText("This device doesnot have a fingerprint sensor");
-                loginbutton.setVisibility(View.GONE);
+                Log.d(TAG, "Biometric not found");
                 break;
 
-            // this means that biometric sensor is not available
             case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
-                msgtex.setText("The biometric sensor is currently unavailable");
-                loginbutton.setVisibility(View.GONE);
+                Log.d(TAG, "Biometric not available");
                 break;
 
-            // this means that the device doesn't contain your fingerprint
             case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
-                msgtex.setText("Your device doesn't have fingerprint saved,please check your security settings");
-                loginbutton.setVisibility(View.GONE);
+                Log.d(TAG, "Biometric not enrolled");
+                Toast.makeText(getApplicationContext(), "Your device doesn't have fingerprint saved,please check your security settings", Toast.LENGTH_LONG).show();
                 break;
         }
-        // creating a variable for our Executor
-        Executor executor = ContextCompat.getMainExecutor(this);
-        // this will give us result of AUTHENTICATION
-        final BiometricPrompt biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+
+
+        Executor biometricExecutor = ContextCompat.getMainExecutor(this);
+        final BiometricPrompt biometricPrompt = new BiometricPrompt(MainActivity.this, biometricExecutor, new BiometricPrompt.AuthenticationCallback() {
             @Override
             public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
                 super.onAuthenticationError(errorCode, errString);
             }
 
-            // THIS METHOD IS CALLED WHEN AUTHENTICATION IS SUCCESS
-            @Override
-            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
-                super.onAuthenticationSucceeded(result);
-                Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
-                loginbutton.setText("Login Successful");
-            }
             @Override
             public void onAuthenticationFailed() {
                 super.onAuthenticationFailed();
+                Log.d(TAG, "Authentication Failed");
             }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Log.d(TAG, "Authentication Succeeded");
+                // Start new activity
+                Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+            }
+
         });
-        // creating a variable for our promptInfo
-        // BIOMETRIC DIALOG
-        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder().setTitle("GFG")
-                .setDescription("Use your fingerprint to login ").setNegativeButtonText("Cancel").build();
+
+        // Biometric prompt
+        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("AUTHENTICATE")
+                .setDescription("Use your fingerprint to login")
+                .setNegativeButtonText("Cancel")
+                .build();
+
+        // Create UI
+        final Button loginbutton = findViewById(R.id.login);
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
